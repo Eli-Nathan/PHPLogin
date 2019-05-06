056cc154200bb905562f0b0f6a7b30a9019d0a67
Eli <ely.nathan93@gmail.com>
2019-05-06 14:43:29 +0100

    Clean table structure

--- index.php
+++ index.php
@@ -1,25 +1,18 @@
 <?php
   require "includes/db.php";
-  $user = true;
-  if($user) {
-    $conn = new Connect("db");
-    $conn->connectMysql();
-    $sql = 'SELECT * FROM users';
-    $result = mysqli_query($conn->connectMysql(), $sql);
+  $conn = new Connect("db");
+  $sql = 'SELECT * FROM users';
+  $result = mysqli_query($conn->connectMysql(), $sql);
 
-    if (mysqli_num_rows($result) > 0) {
-      while($row = mysqli_fetch_assoc($result)) {
-        $firstname = $row["first_name"];
-        $lastname = $row["last_name"];
-        $role = $row["role"];
-        echo "Name: $firstname $lastname <br> User type: $role <br> <br>";
-      }
-    } else {
-      echo "0 results";
+  if (mysqli_num_rows($result) > 0) {
+    while($row = mysqli_fetch_assoc($result)) {
+      $firstname = $row["first_name"];
+      $lastname = $row["last_name"];
+      $role = $row["role"];
+      echo "Name: $firstname $lastname <br> <br>";
     }
-    mysqli_close($conn);
+  } else {
+    echo "No results found";
   }
-  else {
-    echo "You need to login";
-  }
+  mysqli_close($conn);
 ?>
