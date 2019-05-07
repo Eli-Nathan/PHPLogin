8b32401f62645cf62c88beb4cfba9f02c261019d
Eli <ely.nathan93@gmail.com>
2019-05-07 10:44:21 +0100

    Add registration script with salt

--- docs/index.php
+++ docs/index.php
@@ -1,10 +1,19 @@
 <?php
   require "includes/db.php";
-  $conn = new Connect("db");
-  $sql = 'SELECT * FROM users';
-  $result = mysqli_query($conn->connectMysql(), $sql);
+  require "includes/functions.php";
   $pageTitle = "Home";
   include  "includes/header.php";
+
+  if($_POST['email'] && $_POST['password']) {
+    if(userExists($_POST['email'])) {
+      echo "User exists";
+    }
+
+    else {
+      registerUser($_POST['email'], $_POST['password']);
+    }
+  }
+
 ?>
 
 <div class="container">
@@ -14,13 +23,17 @@
     </div>
     <div class="col-sm-10 offset-sm-1 col-md-8 offset-md-2 col-lg-6 offset-lg-3">
       <div class="form ba-1 border border-info bg-white mt-5 p-3 rounded">
-        <form action="#">
+        <form action=<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?> method="post">
+        <?php
+          if($_POST['email'] && $_POST['password']) { ?>
+            <p>Form submitted</p>
+        <?php } ?>
           <label for="validationCustomEmail">Email</label>
           <div class="input-group">
             <div class="input-group-prepend">
               <span class="input-group-text" id="inputGroupPrepend"><i class="fa fa-envelope"></i></span>
             </div>
-            <input type="text" class="form-control" id="validationCustomEmail" placeholder="Email" aria-describedby="inputGroupPrepend" required>
+            <input type="email" class="form-control" name="email" id="validationCustomEmail" placeholder="Email" aria-describedby="inputGroupPrepend" required>
             <div class="invalid-feedback">
               Please enter your email.
             </div>
@@ -31,7 +44,7 @@
             <div class="input-group-prepend">
               <span class="input-group-text" id="inputGroupPrepend"><i class="fa fa-eye"></i></span>
             </div>
-            <input type="password" class="form-control" id="validationCustomPassword" placeholder="Password" aria-describedby="inputGroupPrepend" required>
+            <input type="password" class="form-control" name="password" id="validationCustomPassword" placeholder="Password" aria-describedby="inputGroupPrepend" required>
             <div class="invalid-feedback">
               Please enter your password.
             </div>
---
+++ includes/functions.php
@@ -0,0 +1,44 @@
+<?php
+  // Generate salt for passwords
+  function generateSalt($max = 64) {
+    $characterList = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%&*?";
+    $i = 0;
+    $salt = "";
+    while ($i < $max) {
+        $salt .= $characterList{mt_rand(0, (strlen($characterList) - 1))};
+        $i++;
+    }
+    return $salt;
+  }
+
+  function userExists(&$email) {
+    $conn = new Connect("users");
+    $check = "SELECT email FROM users WHERE email = '$email'";
+    $checkResult = mysqli_query($conn->connectMysql(), $check);
+    if (mysqli_num_rows($checkResult) > 0) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+
+  // Register a user
+  function registerUser(&$email, &$password) {
+    $conn = new Connect("users");
+    $user_salt = generateSalt(); // Generates a salt from the function above
+    $combo = $user_salt . $password; // Appending user password to the salt 
+    $hashed_pwd = hash('sha512',$combo); // Using SHA512 to hash the salt+password combo string
+
+    $insert = "INSERT INTO users(email, salt, pword) VALUES ('$email','$user_salt','$hashed_pwd')";
+
+    $result = mysqli_query($conn->connectMysql(), $insert);
+
+    if($result) {
+      echo "Successful";
+    } else {
+      echo "Could not register user: " . mysql_error();
+    }
+    mysqli_close($conn->connectMysql()); //Closing the connection to the database
+  }
+?>
